7f245b873f1a3ef0f9119791fde2d36a2a8bbcfa
0xdbe <0xdbe@mail.com>
2020-10-28 22:00:46 +0100

    add outputs values

--- ouputs.tf
+++ ouputs.tf
@@ -1,3 +1,7 @@
-output "psql_server_name" {
+output "name" {
   value = azurerm_postgresql_server.main.name
+}
+
+output "login" {
+  value = azurerm_postgresql_active_directory_administrator.main.login
 }
