1971736ee1d0a20b0a9a154bdb258ed556aedde5
0xdbe <0xdbe@mail.com>
2020-10-29 11:57:16 +0100

    add version in outputs values

--- ouputs.tf
+++ ouputs.tf
@@ -2,6 +2,10 @@ output "name" {
   value = azurerm_postgresql_server.main.name
 }
 
+output "version" {
+  value = azurerm_postgresql_server.main.version
+}
+
 output "login" {
   value = azurerm_postgresql_active_directory_administrator.main.login
 }
